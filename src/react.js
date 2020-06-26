fae5d47b6c4d06a60418928f45b4e735ce97aee8
Jas Chen <jaschen.tw@gmail.com>
2020-06-26 10:26:05 +0800

    add build and useOn

--- src/react.js
+++ src/react.js
@@ -2,23 +2,25 @@ import React from 'react';
 
 const Context = React.createContext();
 
-export const Provider = ({ value, children }) => {
-  if (!value) {
+export const Provider = (props) => {
+  if (!props.value) {
     throw new Error('Store is not defined.');
   }
 
-  return <Context.Provider value={value}>{children}</Context.Provider>;
+  return React.createElement(Context.Provider, props);
 };
 
 export const useContainer = () => React.useContext(Context);
 
-export const useStore = (container, key) => {
-  const [state, setState] = React.useState(() => container.get(key));
-
+export const useOn = (container, type, callback) => {
   React.useEffect(() => {
-    container.on(key, setState);
-    return () => container.off(key);
-  }, [key, container]);
+    container.on(type, callback);
+    return () => container.off(type, callback);
+  }, [container, callback]);
+}
 
+export const useStore = (container, type) => {
+  const [state, setState] = React.useState(() => container.get(type));
+  useOn(container, type, setState);
   return state;
 };
